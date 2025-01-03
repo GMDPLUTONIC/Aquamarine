dcdc3935bf1b812b8820d017dcd3a0d3ee2f877b
AcerCo <Elijahsaurusrex0702@gmail.com>
2025-01-03 14:40:31 -0600

    fix main.cpp again

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ class $modify(MenuLayer){
         if (!MenuLayer::init()) return false;
 
         if (!Mod::get()->setSavedValue("seen-intro-popup", true)) {
-            FLAlertLayer* popup = FLAlertLayer::create()
+            FLAlertLayer* popup = FLAlertLayer::create();
                 "Aquamarine",
                 "Welcome to Aquamarine!\n<cb>Press The Tab Key Or Press The Button To Continue Your Voyage.</c>",
                 "OK
